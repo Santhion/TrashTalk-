f707b900dceb54e6978b2264f16ecd3d5735d570
Kevin McReady <kevin.mcready@yahoo.com>
2015-03-05 17:42:08 -0500

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,101 @@
+//
+//  AppDelegate.swift
+//  TrashTalk
+//
+//  Created by Kevin McReady on 2/10/15.
+//  Copyright (c) 2015 EpiphanyApps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        var navigationBarAppearance = UINavigationBar.appearance()
+        
+        navigationBarAppearance.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "It Must Be Destroyed", size: 22)!,  NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+        
+
+        
+        UINavigationBar.appearance().barTintColor = UIColor.blackColor()
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        
+        
+        
+
+        Parse.setApplicationId("gdCuhbaw0Zho7GYGlhjU1UJcrEGHQ3ONxrldlOFa", clientKey: "1jHUWHm2ctVKgONGU1nxRDPpdPtWaf3zdhzaMCvK")
+        
+        
+        let notificationTypes:UIUserNotificationType = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+        let notificationSettings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
+        
+
+        return true
+    }
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        let currentInstallation:PFInstallation = PFInstallation.currentInstallation()
+        currentInstallation.setDeviceTokenFromData(deviceToken)
+        currentInstallation.saveInBackgroundWithTarget(nil, selector: nil)
+    }
+    
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo:NSDictionary!) {
+        var notification:NSDictionary = userInfo.objectForKey("aps") as NSDictionary
+        
+        if(notification.objectForKey("content-available") != nil){
+            if((notification.objectForKey("content-available")?.isEqualToNumber(1)) != nil)
+            {
+                NSNotificationCenter.defaultCenter().postNotificationName("reloadTimeLine", object: nil)
+            }
+            
+            
+        }else{
+            PFPush.handlePush(userInfo)
+        }
+    
+
+    
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        println(error.localizedDescription)
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
